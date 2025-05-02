04fffbee77782aeb8aaac817f2d62181a42e0a74
joaomarcosg <joaomarcos.ipms@gmail.com>
2025-05-02 09:23:28 -0300

    feat: adiciona framework chi e configura servidor

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,40 @@
 package main
+
+import (
+	"encurtador-links/internal/api"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+
+	if err := run(); err != nil {
+		slog.Error("failed to execute code", "error", err)
+		os.Exit(1)
+	}
+
+	slog.Info("all system offline")
+
+}
+
+func run() error {
+
+	handler := api.NewHandler()
+
+	s := http.Server{
+		ReadTimeout:  10 * time.Second,
+		IdleTimeout:  time.Minute,
+		WriteTimeout: 10 * time.Second,
+		Addr:         ":8080",
+		Handler:      handler,
+	}
+
+	if err := s.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
+
+}
---
+++ internal/api/api.go
@@ -0,0 +1,25 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+type apiResponse struct {
+	Error string `json:"error,omitempty"`
+	Data  any    `json:"data,omitempty"`
+}
+
+func NewHandler() http.Handler {
+
+	r := chi.NewMux()
+
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Logger)
+
+	return r
+
+}
